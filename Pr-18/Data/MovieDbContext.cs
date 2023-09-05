2576c8fb8f57d42227109ad88e579c796d43572b
Silambarasan <msilambarasan98@gmail.com>
2023-09-05 17:01:28 +0530

    first commit

---
+++ Pr-18/Data/MovieDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Pr_18.Models;
+
+namespace Pr_18.Data
+{
+    public class MovieDbContext : DbContext
+    {
+        public MovieDbContext (DbContextOptions<MovieDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Pr_18.Models.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ Pr-18/obj/Debug/net6.0/2pe0fbaa.sql
@@ -0,0 +1,29 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+CREATE TABLE [Movie] (
+    [Id] int NOT NULL IDENTITY,
+    [Title] nvarchar(max) NOT NULL,
+    [Genre] nvarchar(max) NOT NULL,
+    [Director] nvarchar(max) NOT NULL,
+    CONSTRAINT [PK_Movie] PRIMARY KEY ([Id])
+);
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20230905105023_MovieMig', N'7.0.10');
+GO
+
+COMMIT;
+GO
+
